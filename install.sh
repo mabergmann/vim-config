430e2ab8b6ec4cb822220526a1c8de745d191328
Matheus Alan Bergmann <mabergmann@inf.ufrgs.br>
2018-12-15 17:24:17 -0200

    Vundle added as submodule and removed from install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 ln ~/.vim/.vimrc ~/.vimrc
-mkdir ~/.vim/bundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 if command -v python3 &>/dev/null; then
 	python3 ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
